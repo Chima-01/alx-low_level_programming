257165aa6729e60036bf38c4f494ccd148ad2da5
Chima-01 <divinechima76@gmail.com>
2023-03-16 16:37:30 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: main -  displaying output with the printf function
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	/*printf function */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
