044f9bd8f39f2a04a7fdf283053f3e6b6063558f
Chima-01 <divinechima76@gmail.com>
2023-03-16 14:40:51 -0700

    programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Using puts() function to print a string
+ */
+
+int main(void)
+{
+	/* declaring a variable and giving a value */
+
+	char programme[] = "Programming is like building a multilingual puzzle";
+
+	puts(programme);
+	return (0);
+}
