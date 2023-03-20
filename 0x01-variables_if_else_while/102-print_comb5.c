cf220d3773d27ae677f9fd3921b2294b363787ed
Chima-01 <divinechima76@gmail.com>
2023-03-20 12:59:10 -0700

    code modified

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,38 +10,28 @@ int main(void)
 {
 	int num;
 	int num1;
-	int num2;
-	int num3;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 98; num++)
 	{
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num1 = num + 1; num1 <= 99; num1++)
 	{
 
-	for (num2 = num; num2 < 10; num2++)
-	{
-
-	for (num3 = num1 + 1; num3 < 10; num3++)
-	{
-		putchar('0' + num);
-		putchar('0' + num1);
+		putchar('0' + (num / 10));
+		putchar('0' + (num % 10));
 
 		putchar(' ');
 
-		putchar('0' + num2);
-		putchar('0' + num3);
+		putchar('0' + (num1 / 10));
+		putchar('0' + (num1 % 10));
 
-	if (num == 9 && num1 == 8)
-	{
-		if (num2 == 9 && num3 == 9)
+		if (num == 98 && num1 == 99)
+		{
 			break;
-	}
+		}
 
 		putchar(',');
 		putchar(' ');
-	}
-	}
 	}
 	}
 		putchar('\n');
