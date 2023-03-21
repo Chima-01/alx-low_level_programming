d88358beb4ea8a15946667065501d3ce714fbe77
Chima-01 <divinechima76@gmail.com>
2023-03-21 11:05:56 -0700

    function declaration of _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Description: main - A function Declaration, display to stdout '_putchar'
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
