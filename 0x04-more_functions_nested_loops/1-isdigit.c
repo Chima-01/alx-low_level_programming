ba7d28f677c4f4d76fffcc3d2910d9f71de16b70
Chima-01 <divinechima76@gmail.com>
2023-03-23 13:21:21 -0700

    a function that checks if a value is a number or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * Description: _isdigit - This function returns 1 and 0
+ * if an argument is a number or not
+ * @c: parameter passed to function
+ * Return: 1 0r 0
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
