2a5974e40239062eb8af0d343f951e05601e8126
Chima-01 <divinechima76@gmail.com>
2023-03-23 11:15:41 -0700

    fixed the bug in the files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: main - This function checks if int is positive or negative
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 45;
+	positive_or_negative(i);
+
+	return (0);
+}
