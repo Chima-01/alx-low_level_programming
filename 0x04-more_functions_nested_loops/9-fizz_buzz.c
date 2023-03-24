4976a27a653c842651039589e01792ed9e6108d4
Chima-01 <divinechima76@gmail.com>
2023-03-23 18:19:26 -0700

    did some fizzbuzz exercise

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - This out prints FizzBuzz
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((num % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+			printf("\n");
+			return (0);
+}
