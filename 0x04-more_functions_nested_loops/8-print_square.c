3495fe886dee9791d0acf779ca7d000ea4d229d4
Chima-01 <divinechima76@gmail.com>
2023-03-24 20:57:16 -0700

    displaying square according to arguments given

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - This function display arguments in squares
+ * @size: parameter given
+ * Return: 0 (success)
+ */
+
+void print_square(int size)
+{
+	int sq1;
+	int sq2;
+
+	for (sq1 = 1; sq1 <= size; sq1++)
+	{
+	for (sq2 = 1; sq2 <= size; sq2++)
+	{
+		_putchar('#');
+	}
+		_putchar('\n');
+	}
+}
