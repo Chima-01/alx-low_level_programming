3f3488eb5793de91f9e4795d281960b210cf4898
Chima-01 <divinechima76@gmail.com>
2023-03-27 12:44:08 -0700

    modified 1-swap file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * swap_int - A function that displays double values of a parameter
+ * swap_int - A function that displays swaps values of an argument
  * @a: first parameter given
  * @b: second parameter
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+
 }
