8f39ca9a09b437998aeacc6af87d830f851ab9d0
Chima-01 <divinechima76@gmail.com>
2023-03-27 11:15:38 -0700

    a function that display double values of a paramter

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that displays double values of a parameter
+ * @a: first parameter given
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
