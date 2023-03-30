93117be1f6e8a3a70b8deaa20c2f3d57381b1835
Chima-01 <divinechima76@gmail.com>
2023-03-30 11:52:55 -0700

    The strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - This funtion copies from the source to the
+ * destination according to the number of char specified
+ * The second while loops is executed if n is larger than
+ * the src buffer
+ * @dest: destination of copied char
+ * @src: source to be copied
+ * @n: number of character to be copied
+ * Return: char (success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	if (src == NULL || n == 0)
+	{
+		return (NULL);
+	}
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+		dest[i] = '\0';
+
+		return (dest);
+}
