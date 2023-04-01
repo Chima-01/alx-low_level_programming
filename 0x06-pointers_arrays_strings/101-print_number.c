eeacd51260f025eee46f2f9a3a83964c05535b66
Chima-01 <divinechima76@gmail.com>
2023-04-01 13:01:55 -0700

    a function that displays numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,14 @@ void print_number(int n)
 {
 	if (n < -9)
 	{
-		n = n * - 1;
+		n = n * -1;
 		_putchar('-');
 		print_number(n / 10);
 		_putchar('0' + (n % 10));
 	}
 	else if (n < 0)
 	{
-		n = n * - 1;
+		n = n * -1;
 		 _putchar('-');
 		 _putchar('0' + (n % 10));
 	}
