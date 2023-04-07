71f958a5bca9266e0006156549472164f1c0fca3
Chima-01 <divinechima76@gmail.com>
2023-04-06 23:57:39 -0700

    The pow function using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This functions a numx raise to power of a numy
+ * @x: first parameter num
+ * @y: number to power of x
+ * Return: 0 (success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (x && y < 0)
+		return (-1);
+	else if (x && y == 0)
+		return (1);
+	else
+	{
+	return (x *  _pow_recursion(x, y - 1));
+	}
+}
