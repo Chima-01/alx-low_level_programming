8b6591adbf92efedda710a9cfb35ab03db04e789
Chima-01 <divinechima76@gmail.com>
2023-04-09 08:21:26 -0700

    a function thats adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This function add all negative number in an argument
+ * @argc: counts argument
+ * @argv: displays values
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		result += atoi(argv[i]);
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+			printf("%d\n", result);
+			return (0);
+}
