2bd741db8dc20e2e009576faf8740e381ac5ae97
Chima-01 <divinechima76@gmail.com>
2023-04-10 15:33:53 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - The function copies an input from a string
+ * to another
+ * @dest: destination of copied str
+ * @src: source to copy
+ * Return: Str (success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+		return (dest);
+}
