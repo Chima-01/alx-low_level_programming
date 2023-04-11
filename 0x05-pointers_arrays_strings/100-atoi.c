ea81f29ed7b26c97b066f96f5b7aba65e8012794
Chima-01 <divinechima76@gmail.com>
2023-04-10 18:29:32 -0700

    atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,8 @@
 
 int _atoi(char *s)
 {
-	int i = 0, result = 0, convert = 1;
-	int si = 0;
+	int i = 0, convert = 1, j = 0;
+	unsigned int result = 0;
 
 	while (s[i])
 	{
@@ -20,14 +20,15 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		si = 1;
+		j = 1;
 		result = (result * 10) + (s[i] - '0');
 		i++;
 	}
-	if (si == 1)
+	if (j == 1)
 	{
 		break;
 	}
+
 		i++;
 	}
 		result *= convert;
