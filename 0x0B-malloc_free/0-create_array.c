0f12fc11ab5340bcd4eecad779f99723e03655b6
Chima-01 <divinechima76@gmail.com>
2023-04-12 12:19:50 -0700

    create an array with malloc allocating the memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - This function creates an array of char
+ * and allocates a memery for the characters using malloc function
+ * @size: size of memory to be allocated time char
+ * @c: char to be stored
+ * Return: a pointer to the memory of char(success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	str = malloc((size + 1) * sizeof(char));
+
+		if (size == 0 || str == NULL)
+			return (NULL);
+
+		while (i < size)
+		{
+			str[i] = c;
+			i++;
+		}
+			str[i] = '\0';
+
+		return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+Dynamic Memmory Allocation
+The malloc function and free
+Malloc function is used to dynamically store up a size of memory
+it return a pointer to the base of the memory
+
+free function is used to free memories created dynamcally
+like memory created by the malloc function
