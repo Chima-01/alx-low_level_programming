9b2e807fb2b645f78d6b4447608e96d86d6b1445
Chima-01 <divinechima76@gmail.com>
2023-04-20 15:58:15 -0700

    a function that prints the numbers given as argument

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - This function prints numbers
+ * @separator: seprates each numbers with comma
+ * @n: number of argument to be passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	if (separator == NULL || n == 0)
+		return;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, unsigned int));
+		if (i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+		printf("\n");
+
+	va_end(list);
+}
