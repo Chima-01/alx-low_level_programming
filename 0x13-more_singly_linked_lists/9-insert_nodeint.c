371d10aadf701c7d46633b02a2377b242c25d724
Chima-01 <divinechima76@gmail.com>
2023-05-03 13:17:57 -0700

    fixed bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,9 +36,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 		i++;
 	}
+		if (*head == NULL && i != idx - 1)
+		{
+			return (NULL);
+		}
 		add_node->next = (*head)->next;
 		(*head)->next = add_node;
 	}
-
 		return (*head);
 }
