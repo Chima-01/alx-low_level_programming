b958926965726c6fcdad85dca3cdc67594b6e543
Chima-01 <divinechima76@gmail.com>
2023-05-07 09:07:23 -0700

    updated binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -26,11 +26,13 @@ int _strlen(const char *str)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0, base = 1;
-	int i, len = _strlen(b);
+	unsigned int dec = 0;
+	int i, len, base = 1;
 
 	if (b == NULL)
 		return (0);
+
+	len = _strlen(b);
 
 	for (i = len - 1; i >= 0; i--)
 	{
