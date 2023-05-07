0eef4682807e90014de1d103397fd7c89e5e4acf
Chima-01 <divinechima76@gmail.com>
2023-05-07 08:47:37 -0700

    a function that converts a binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - This function checks the length of a str
+ * @str: str parameter
+ * Return: str length
+ */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+		return (i);
+}
+
+/**
+ * binary_to_uint - This function converts a binary
+ * to decimal
+ * @b: str parameter
+ * Return: integer in base 10
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, base = 1;
+	int i, len = _strlen(b);
+
+	if (b == NULL)
+		return (0);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] == '1')
+			{
+				dec += base;
+			}
+				base *= 2;
+		}
+		else
+			return (0);
+	}
+		return (dec);
+}
