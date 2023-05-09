0b5b8150a507892848f66bee4de166a51f8dcd3f
Chima-01 <divinechima76@gmail.com>
2023-05-09 14:37:15 -0700

    fixed bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
+		close(fd);
+		return (-1);
+	}
 		len = strlen(text_content);
 	i = write(fd, text_content, len);
 	if (!i)
@@ -31,12 +34,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	}
-	else
-	{
-		close(fd);
-		return (-1);
-	}
+
 	close(fd);
 	return (1);
 }
