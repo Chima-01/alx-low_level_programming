9424f2ba3fa225e83519b54ca50e9471e494e8b1
Chima-01 <divinechima76@gmail.com>
2023-06-19 18:41:24 -0700

    run a c script from python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * Thie functions below is an arithmetic library
+ * explicitly created
+ * The functions below are to be used in a python script
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+int div(int a, int b)
+{
+	return a / b;
+}
+
+int mod(int a, int b)
+{
+	return a % b;
+}
