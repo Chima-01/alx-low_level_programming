a418865170ae1e2a60f0be1610080977ba0a8b4f
Chima-01 <divinechima76@gmail.com>
2023-07-08 12:59:26 -0700

    a function to create and insert an hash node

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,6 +36,8 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+hash_node_t *create_node(hash_node_t *head, const char *key, const char *value);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 void free_table(hash_table_t *table);
 #endif
